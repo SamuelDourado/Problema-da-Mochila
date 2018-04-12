28d6a9783ce3636f8af4736b6173ad68c5878886
Samuel Dourado <SD@localhost.localdomain>
2018-04-12 10:02:32 -0300

    Foi criado a class carregador

---
+++ src/Carregador.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Carregador {
+	protected ArrayList<Item> allItens = new ArrayList<Item>();
+	
+	public void LerItens() {
+		Boolean stop = false;
+		Scanner entrada = new Scanner(System.in);
+		while(!stop) {
+			System.out.println("* Adicionar Item:");
+			
+			System.out.print("** Nome:");
+			String nome = entrada.next();
+			
+			System.out.print("** Valor:");
+			float value = entrada.nextFloat();
+			
+			System.out.print("** Peso:");
+			float peso = entrada.nextFloat();
+			
+			allItens.add( new Item(nome, value, peso) );
+			System.out.print("** Parar:");
+			stop = entrada.nextBoolean();
+			System.out.println("*****");
+		}
+	}
+	
+	public Mochila Guloso(){
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Informe o tamanho da mochila:");
+		float size = entrada.nextFloat();
+		Mochila mochila = new Mochila(size);
+		
+		for (Item Item: this.allItens) {
+			Item maxItem = this.maxItem();
+			mochila.In(maxItem);
+			maxItem.adicionado = true;
+			//allItens.remove(maxItem);
+		}
+		
+		return mochila;
+	}
+	
+	public Item maxItem() {
+		Item maxItem = null;
+		for (Item Item: this.allItens) {
+			if(maxItem == null || (!Item.adicionado && Item.getValue() > maxItem.getValue())){
+				maxItem = Item;
+			}
+		}
+		return maxItem;
+	}
+	
+	public void printMochila(Mochila mochila) {
+		for (Item Item: mochila.Listar()) {
+			System.out.print(Item.getName() + ' ');
+		}
+		System.out.println();
+	}
+	
+	public float getTotalValorMochila(Mochila mochila) {
+		float total = 0;
+		for (Item Item: mochila.Listar()) {
+			total = Item.getValue() + total;
+		}
+		return total;
+	}
+	
+	public float getTotalPesoMochila(Mochila mochila) {
+		float peso = 0;
+		for (Item Item: mochila.Listar()) {
+			peso = Item.getPeso() + peso;
+		}
+		return peso;
+	}
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -4,65 +4,23 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<Item> allItens = new ArrayList<Item>();
-		Boolean stop = false;
-		
-		Scanner entrada = new Scanner(System.in);
-		while(!stop) {
-			System.out.println("Adicionar Item:");
-			
-			System.out.print("Nome:");
-			String nome = entrada.next();
-			
-			System.out.print("Valor:");
-			float value = entrada.nextFloat();
-			
-			System.out.print("Peso:");
-			float peso = entrada.nextFloat();
-			
-			allItens.add( new Item(nome, value, peso) );
-			stop = entrada.nextBoolean();
-			System.out.println("___");
-		}
-		//entrada.close();
-
-		Main res = new Main();
-		Mochila mochila = res.Guloso(allItens);
-		System.out.println("guloso");
-		res.printMochila(mochila);
+		Carregador carregador = new Carregador();
+		carregador.LerItens();
+		Mochila m1 = carregador.Guloso();
+		carregador.printMochila(m1);
 	}
 	
-	public Item maxItem(ArrayList<Item> Itens) {
-		Item maxItem = null;
-		for (Item Item: Itens) {
-			if(maxItem == null || (!Item.adicionado && Item.getValue() > maxItem.getValue())){
-				maxItem = Item;
-			}
+	public float Dinamico(int i, int s,float[][] res, ArrayList<Item> allItens) {
+		if(i == 0 || s == 0) {
+			return 0;
 		}
-		return maxItem;
-	}
-	
-	public Mochila Guloso(ArrayList<Item> allItens){
-		Scanner entrada = new Scanner(System.in);
-		System.out.print("Informe o tamanho da mochila:");
-		float size = entrada.nextFloat();
-		Mochila mochila = new Mochila(size);
-		
-		for (Item Item: allItens) {
-			Item maxItem = this.maxItem(allItens);
-			mochila.In(maxItem);
-			maxItem.adicionado = true;
-			//allItens.remove(maxItem);
+		Item novoItem = allItens.get(i+1);
+		float novo = (novoItem.getValue() + this.Dinamico(i, (int) (s - novoItem.getPeso()) , res, allItens));
+		float old = this.Dinamico(i-1, s, res, allItens);
+		if(novo > old) {
+			return novo;
 		}
-		
-		return mochila;
-	}
-	
-	public void printMochila(Mochila mochila) {
-		for (Item Item: mochila.Listar()) {
-			System.out.println(Item.getName());
-		}
+		return old;
 	}
 
 }
